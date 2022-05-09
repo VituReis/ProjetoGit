5b1551daa6dfea41cbd167629cd9012ebca93e60
VituReis <98780732+VituReis@users.noreply.github.com>
2022-05-09 12:12:51 -0300

    Add files via upload

---
+++ par_impar.js
@@ -0,0 +1,11 @@
+for (let Alunos = 0; Alunos <= 17; Alunos++) {
+    if (Alunos == 0) {
+        console.log("zero " + Alunos);
+    } else {
+        if(Alunos % 2 ==0) {
+            console.log('par ' + Alunos);
+        } else {
+            console.log("impar " + Alunos);
+        }    
+   }
+}
